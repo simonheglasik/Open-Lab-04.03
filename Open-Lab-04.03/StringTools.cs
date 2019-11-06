db0ebbc01f3c814014a3b018b1355af9d573626e
simonheglasik <simonheglasik@gmail.com>
2019-11-06 13:57:04 +0100

    04.03

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -6,7 +6,13 @@ namespace Open_Lab_04._03
     {
         public string RemoveFirstLast(string original)
         {
-            throw new NotImplementedException();
+            {
+                if (original == "") return original;
+                string a = original.Remove(original.Length-1);
+                if (a == "") return original;
+                string b = a.Remove(0, 1);
+                return b;
+            }
         }
     }
 }
